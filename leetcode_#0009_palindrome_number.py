b241d223407aca3353981a6c380ab4c0356373ed
edalisay <edalisay.code@gmail.com>
2025-05-02 23:56:30 +0800

    Leetcode #0009 & #0605#

---
+++ leetcode_#0009_palindrome_number.py
@@ -0,0 +1,59 @@
+# ------------------------------------------------------------------------------------------------- #
+# --------------------------------------- PROBLEM STATEMENT --------------------------------------- #
+# ------------------------------------------------------------------------------------------------- #
+
+''' 9. Palindrome Number
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+
+Follow up: Could you solve it without converting the integer to a string?
+
+'''
+
+
+# ------------------------------------------------------------------------------------------------- #
+# ------------------------------------------ PYTHON CODE ------------------------------------------ #
+# ------------------------------------------------------------------------------------------------- #
+
+
+class Solution:
+    def isPalindrome(self, x):
+        self.x = x
+
+        is_palindrome = False
+        reversed_x = ''.join(reversed(str(self.x)))
+        if str(self.x) == reversed_x:
+            is_palindrome = True
+
+        return is_palindrome
+
+
+# ------------------------------------------------------------------------------------------------- #
+# ------------------------------------------ TEST CASES ------------------------------------------- #
+# ------------------------------------------------------------------------------------------------- #
+x = 121
+s = Solution()
+print(s.isPalindrome(x))
